bb945a88553e42892f320c0bc9a3cd03cf2e9eab
Silas Joisten <silasjoisten@proton.me>
2024-05-14 19:05:00 +0300

    Enhancement: Adds CachingClient

--- src/StoryblokClient.php
+++ src/StoryblokClient.php
@@ -15,7 +15,6 @@ namespace SensioLabs\Storyblok\Api;
 
 use OskarStark\Value\TrimmedNonEmptyString;
 use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 use Webmozart\Assert\Assert;
@@ -23,7 +22,7 @@ use Webmozart\Assert\Assert;
 /**
  * @author Silas Joisten <silasjoisten@proton.me>
  */
-final class StoryblokClient
+final class StoryblokClient implements StoryblokClientInterface
 {
     private HttpClientInterface $client;
     private string $token;
@@ -31,29 +30,16 @@ final class StoryblokClient
 
     public function __construct(
         string $baseUri,
-        #[\SensitiveParameter] string $token,
+        #[\SensitiveParameter]
+        string $token,
         int $timeout = 4,
-        ?HttpClientInterface $storyblokClient = null
+        ?HttpClientInterface $storyblokClient = null,
     ) {
         $this->client = $storyblokClient ?? HttpClient::createForBaseUri($baseUri);
         $this->token = TrimmedNonEmptyString::fromString($token, '$token must not be an empty string')->toString();
         $this->timeout = $timeout;
     }
 
-    /**
-     * Requests an HTTP resource.
-     *
-     * Responses MUST be lazy, but their status code MUST be
-     * checked even if none of their public methods are called.
-     *
-     * Implementations are not required to support all options described above; they can also
-     * support more custom options; but in any case, they MUST throw a TransportExceptionInterface
-     * when an unsupported option is passed.
-     *
-     * @param array<mixed> $options
-     *
-     * @throws TransportExceptionInterface When an unsupported option is passed
-     */
     public function request(string $method, string $url, array $options = []): ResponseInterface
     {
         Assert::notStartsWith($url, 'http', '$url should be relative: Got: %s');
